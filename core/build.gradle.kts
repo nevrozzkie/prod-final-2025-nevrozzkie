774f5bc2bb46ab126b164a9d375269373ac9745d
nevrozza <artemker667@gmail.com>
2025-02-22 09:50:53 +0300

    ура, можно и поспать...

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -8,7 +8,6 @@ dependencies {
     api(libs.koin.android)
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.cio)
-    implementation(libs.ktor.client.android)
     implementation(libs.ktor.client.json)
     implementation(libs.ktor.client.negotiation)
     implementation(libs.ktor.client.logging)
--- core/src/main/kotlin/ktor/HttpClientFactory.kt
+++ core/src/main/kotlin/ktor/HttpClientFactory.kt
@@ -4,14 +4,9 @@ import android.content.Context
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.call.HttpClientCall
-import io.ktor.client.engine.HttpClientEngineConfig
-import io.ktor.client.engine.android.Android
-import io.ktor.client.engine.android.AndroidClientEngine
-import io.ktor.client.engine.android.AndroidEngineConfig
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.engine.cio.CIOEngineConfig
 import io.ktor.client.plugins.DefaultRequest
-import io.ktor.client.plugins.HttpResponseValidator
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.cache.storage.FileStorage
@@ -21,12 +16,8 @@ import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.SIMPLE
-import io.ktor.client.request.header
-import io.ktor.client.request.headers
 import io.ktor.client.statement.HttpReceivePipeline
 import io.ktor.client.statement.HttpResponse
-import io.ktor.client.statement.content
-import io.ktor.http.CacheControl
 import io.ktor.http.ContentType
 import io.ktor.http.Headers
 import io.ktor.http.HeadersBuilder
@@ -34,19 +25,13 @@ import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpProtocolVersion
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.URLProtocol
-import io.ktor.http.contentType
-import io.ktor.http.maxAge
 import io.ktor.serialization.kotlinx.json.json
-import io.ktor.util.appendAll
 import io.ktor.util.date.GMTDate
-import io.ktor.util.filter
-import io.ktor.util.toMap
 import io.ktor.utils.io.ByteReadChannel
 import io.ktor.utils.io.InternalAPI
 import kotlinx.serialization.json.Json
 import java.io.File
 import kotlin.coroutines.CoroutineContext
-import kotlin.math.max
 
 class HttpClientFactory {
     fun createFactory(
